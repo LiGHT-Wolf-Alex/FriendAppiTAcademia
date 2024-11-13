338da3b3c1e203909435b891b889967aff4a8f62
LIGHT WolF <aleks.valmus2001@gmail.com>
2024-11-13 23:55:10 +0300

    Add friend entity model

---
+++ FriendApp.Entities/Models/Friend.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FriendApp.Entities.Models;
+
+public class Friend
+{
+    public int FriendID { get; set; }
+
+    [StringLength(40, ErrorMessage = "Имя должно быть не более 40 символов")]
+    public required string FriendName { get; set; }
+
+    [StringLength(100, ErrorMessage = "Место не должно быть более 100 символов")]
+    public string? Place { get; set; }
+}
