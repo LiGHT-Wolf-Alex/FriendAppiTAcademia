721bbade5e42c3b24e8745a4a18628c5905a56d1
LIGHT WolF <aleks.valmus2001@gmail.com>
2024-11-14 01:50:48 +0300

    added the FriendRepository.cs file and a partial implementation of the methods

---
+++ FriendApp.BLL/Services/FriendService.cs
@@ -0,0 +1,34 @@
+﻿using FriendApp.BLL.Interfaces;
+using FriendApp.Entities.Models;
+
+namespace FriendApp.BLL.Services;
+
+public class FriendService(IFriendService friendService) : IFriendService
+{
+    private readonly IFriendService _friendService = friendService;
+
+    public List<Friend> GetAllFriends()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddFriend(Friend friend)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateFriend(Friend friend)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteFriend(int friendId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Friend GetFriendById(int friendId)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ FriendApp.DAL/Repositories/FriendRepository.cs
@@ -0,0 +1,40 @@
+﻿using FriendApp.DAL.interfaces;
+using FriendApp.Entities.Models;
+
+namespace FriendApp.DAL.Repositories;
+
+public class FriendRepository : IFriendRepository
+{
+    private static List<Friend> _friends =
+    [
+        new() { FriendID = 1, FriendName = "John Doe", Place = "New York" },
+        new() { FriendID = 2, FriendName = "Jane Smith", Place = "Los Angeles" },
+        new() { FriendID = 3, FriendName = "Alice Johnson", Place = "Chicago" },
+        new() { FriendID = 4, FriendName = "Bob Brown", Place = "San Francisco" }
+    ];
+
+    public List<Friend> GetAll()
+    {
+        return _friends;
+    }
+
+    public void Add(Friend friend)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Friend friend)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int friendId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Friend GetById(int friendId)
+    {
+        throw new NotImplementedException();
+    }
+}
